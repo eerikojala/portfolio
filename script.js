98eb12af1ea461e6e7ad59ce0a7fdc47a22d75ff
eerik ojala <eerik.ojala@edu.tampere.fi>
2024-01-08 14:56:22 +0200

    pushattu

---
+++ script.js
@@ -0,0 +1,27 @@
+function scrollHero() {
+    const element = document.getElementById("hero");
+    element.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+
+function scrollAbout() {
+    const element = document.getElementById("about");
+    element.scrollIntoView({
+	    behavior: "smooth",
+    });
+}
+
+function scrollPortfolio() {
+    const element = document.getElementById("portfolio");
+    element.scrollIntoView({
+	    behavior: "smooth",
+    });
+}
+
+function scrollContact() {
+    const element = document.getElementById("contact_container");
+    element.scrollIntoView({
+	    behavior: "smooth",
+    });
+}
